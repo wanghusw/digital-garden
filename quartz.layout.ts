4fc2da5d81e86632bdb62ce128afc6b4c3fd929f
WangHu <wanghusw@gmail.com>
2026-01-01 21:45:40 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,28 +1,28 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
-const RecentNotes = [
+const recentNotes = [
   Component.RecentNotes({
-      title: "最近的笔记",
-      limit: 3,
-      showTags: false,
-      filter: (f) => f.slug!.startsWith("Post/"),
-      linkToMore: "Post/" // 确保 /notes 页面存在
-    }),
-    Component.RecentNotes({
-      title: "最近的日记",
-      limit: 3,
-      showTags: false,
-      filter: (f) => f.slug!.startsWith("Journals/"),
-      linkToMore: "/Journals" // 确保 /notes 页面存在
-    }),
+    title: "Recent Writing",
+    limit: 4,
+    showTags: false,
+    filter: (f) => f.slug!.startsWith("Post/"),
+    linkToMore: "Post/" // 确保 /notes 页面存在
+  }),
+  Component.RecentNotes({
+    title: "Recent Notes",
+    limit: 3,
+    filter: (f) => f.slug!.startsWith("Journals/"),
+    linkToMore: "Journals/" as SimpleSlug,
+  }),
 ]
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [...recentNotes.map((c) => Component.MobileOnly(c))],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/wanghusw/digital-garden",
@@ -30,53 +30,25 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+const left = [
+  Component.PageTitle(),
+  Component.MobileOnly(Component.Spacer()),
+  Component.Flex({
+    components: [
+      {
+        Component: Component.Search(),
+        grow: true,
+      },
+      { Component: Component.Darkmode() },
+    ],
+  }),
+  ...recentNotes.map((c) => Component.DesktopOnly(c)),
+]
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  afterBody: [
-    /*Component.RecentNotes({
-      title: "最近的笔记",
-      limit: 3,
-      showTags: false,
-      linkToMore: "/Inbox" // 确保 /notes 页面存在
-    })*/
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-      Component.RecentNotes({
-      title: "最近的笔记",
-      limit: 3,
-      showTags: false,
-      filter: (f) => f.slug!.startsWith("Post/"),
-      linkToMore: "Post/" // 确保 /notes 页面存在
-    }),
-    Component.RecentNotes({
-      title: "最近的日记",
-      limit: 3,
-      showTags: false,
-      filter: (f) => f.slug!.startsWith("Journals/"),
-      linkToMore: "/Journals" // 确保 /notes 页面存在
-    }),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+  left,
   right: [
     Component.Graph({
       localGraph: {
@@ -93,20 +65,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
+  left,
   right: [],
 }
