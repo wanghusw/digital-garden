79e21c6077f10a491f1ad1974df51dfa35d80388
WangHu <wanghusw@gmail.com>
2026-06-14 18:00:04 +0800

    Change font to LXGW Wenkai

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -61,6 +61,8 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.7.0/style.css" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
